baeefc6b93ae76ccc5a466d8090c70733b920dc8
Simon Convent <simon@convnet.de>
2023-02-06 19:38:22 +0100

    Get info on more package managers, other configs and activity

--- read_advanced_repo_info.py
+++ read_advanced_repo_info.py
@@ -22,31 +22,68 @@ def read_advanced_repo_info(github_client: Github, repos):
             # requirements.txt (pip)
             repo.requirements_txt_files = find_files(github_client, repo, ["Python"], "requirements.txt")
 
-            # TODO: Add the following package managers:
-            # bundler
-            # cargo
-            # composer
-            # docker
-            # mix
-            # elm
-            # git submodules
-            # github actions (/.github/workflows/*.yaml)
-            # gomod
-            # nuget
-            # pub
-            # terraform
+            # Gemfile (bundler)
+            repo.gemfile_files = find_files(github_client, repo, ["Ruby"], "Gemfile")
 
-            # TODO: Get info whether repo already has Dependabot config
+            # Cargo.toml (cargo)
+            repo.cargo_toml_files = find_files(github_client, repo, ["Rust"], "Cargo.toml")
+
+            # composer.json (composer)
+            repo.composer_json_files = find_files(github_client, repo, ["PHP"], "composer.json")
+
+            # Dockerfile (docker)
+            repo.dockerfile_files = find_files(github_client, repo, ["Dockerfile"], "Dockerfile")
+
+            # mix.exs (mix)
+            repo.mix_exs_files = find_files(github_client, repo, ["Elixir"], "mix.exs")
+
+            # elm.json (elm)
+            repo.elm_json_files = find_files(github_client, repo, ["Elm"], "elm.json")
+
+            # .gitmodules (git submodules)
+            # TODO: Match all languages
+            repo.gitmodules_files = find_files(github_client, repo, [""], ".gitmodules")
+
+            # .github/workflows/*.yaml (github actions)
+            # TODO: Match all languages
+            repo.github_workflows_files = find_files(github_client, repo, [""], ".github/workflows")
+
+            # go.mod (gomod)
+            repo.go_mod_files = find_files(github_client, repo, ["Go"], "go.mod")
+
+            # *.csproj (nuget)
+            # TODO: Potentially match more languages
+            # TODO: Check whether this is the correct filename
+            # TODO: Match with regex
+            repo.csproj_files = find_files(github_client, repo, ["C#"], "*.csproj")
+
+            # pubspec.yaml (pub)
+            repo.pubspec_yaml_files = find_files(github_client, repo, ["Dart"], "pubspec.yaml")
+
+            # *.tf (terraform)
+            # TODO: Match with regex
+            repo.tf_files = find_files(github_client, repo, ["Terraform"], "*.tf")
+
+            # Get info whether repo already has Dependabot config
+            repo.dependabot_config_files = find_files(github_client, repo, [""], ".github/dependabot.yml")
+            repo.has_dependabot_config = len(repo.dependabot_config_files) > 0
+
             # TODO: Get info whether Dependabot has opened PRs
             #repo_info.get_pulls
-            # TODO: Get info whether repo has Renovate config
-            # TODO: Get info on activity of repo
+
+            # Get info whether repo has Renovate config
+            repo.renovate_config_files = find_files(github_client, repo, [""], ".github/renovate.json")
+            repo.has_renovate_config = len(repo.renovate_config_files) > 0
+
+            # Get info on activity of repo
+            repo.activity = repo_info.get_stats_participation().all
             
             repo.has_advanced_info = True
         else:
             print(f"Advanced info for repo {repo.full_name} already read")
         
 
+# TODO: Match with regex
 def find_files(github_client, repo, languages, filename):
     # Check if repo uses any of the given languages
     languages_intersection = [language for language in languages if language in repo.languages]
