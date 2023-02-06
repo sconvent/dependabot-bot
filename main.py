ed9a2012a9d129d3edaa896454aa9c23c76ee829
Simon Convent <simon@convnet.de>
2023-02-06 21:49:38 +0100

    Make backwards compatible

--- main.py
+++ main.py
@@ -30,21 +30,20 @@ else:
     github_client = Github(per_page=100, login_or_token=token)
 
 arguments = sys.argv
-match arguments[1]:
-    # Read basic repo info for relevant repos
-    case "read_basic_repo_info":
-        max_count = int(arguments[2])
-        read_basic_repo_info(github_client, repos, max_count)
-    case "read_advanced_repo_info":
-        read_advanced_repo_info(github_client, repos)
-    case "create_forks_and_prs":
-        create_forks_and_prs(github_client, repos)
-    case "create_real_prs":
-        create_real_prs(github_client, repos)
-    case "dump_db":
-        dump_db(github_client, repos)
-    case _:
-        print("Invalid argument")
+command = arguments[1]
+if command == "read_basic_repo_info":
+    max_count = int(arguments[2])
+    read_basic_repo_info(github_client, repos, max_count)
+elif command == "read_advanced_repo_info":
+    read_advanced_repo_info(github_client, repos)
+elif command ==  "create_forks_and_prs":
+    create_forks_and_prs(github_client, repos)
+elif command ==  "create_real_prs":
+    create_real_prs(github_client, repos)
+elif command  == "dump_db":
+    dump_db(github_client, repos)
+else:
+    print("Invalid argument")
 
 # save local_db
 with(open('local_db', 'wb')) as f:
