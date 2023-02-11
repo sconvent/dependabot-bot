b012c9ce40032f6dcaaf1a8da67706c926d00583
Simon Convent <simon@convnet.de>
2023-02-11 21:02:17 +0100

    Clone instead of download

--- read_advanced_repo_info.py
+++ read_advanced_repo_info.py
@@ -1,36 +1,29 @@
 from github import Github
-import time
-import requests
-import zipfile
 import os
+import pathlib
 
 def read_advanced_repo_info(github_client: Github, repos):
+    # create folder if not exists
+    try:
+        os.mkdir(f"repos")
+    except:
+        pass
+
     for repo in repos.values():
         if repo.has_advanced_info and len(repo.languages) > 0:
 
-            # Download if not already downloaded
-            try:
-                with open(f"repos/{repo.full_name}.zip", "rb") as f:
-                    print(f"Repo {repo.full_name} already downloaded")
-            except:
-                print(f"Downloading repo {repo.full_name}")
-                link = f"https://github.com/{repo.full_name}/archive/refs/heads/{repo.default_branch}.zip"
-                content = requests.get(link, allow_redirects=True).content
+            # Check if folder exists
+            folder_path = f"repos/{repo.full_name.replace('/', '_')}"
+            if os.path.isdir(folder_path):
+                print(f"Repo {repo.full_name} already cloned. Will pull instead.")
+                command = f"cd {folder_path} && git pull"
+                os.system(command)
+            else:
+                print(f"Cloning repo {repo.full_name}")
 
-                # create folder if not exists
-                try:
-                    os.mkdir(f"repos")
-                except:
-                    pass
-
-                # save file
-                file_name = f"repos/{repo.full_name.replace('/', '_')}.zip"
-                with open(file_name, "wb") as f:
-                    f.write(content)
-
-                # unzip file
-                with zipfile.ZipFile(file_name, 'r') as zip_ref:
-                    zip_ref.extractall(f"repos/{repo.full_name}")
+                folder_path = f"repos/{repo.full_name.replace('/', '_')}"
+                command = f"git clone --depth 1 https://github.com/{repo.full_name}.git {folder_path}"
+                os.system(command)
 
             print(f"Reading advanced info for repo {repo.full_name}")
             repo_info = github_client.get_repo(repo.full_name)
@@ -39,68 +32,68 @@ def read_advanced_repo_info(github_client: Github, repos):
 
             # package.json (npm, yarn)
             # TODO: Match more languages
-            repo.package_json_files = find_files(github_client, repo, ["JavaScript", "TypeScript"], "package.json")
+            repo.package_json_files = find_files(repo, ["JavaScript", "TypeScript"], "package.json")
             
             # pom.xml (maven)
-            repo.pom_xml_files = find_files(github_client, repo, ["Java", "Kotlin", "Scala"], "pom.xml")
+            repo.pom_xml_files = find_files(repo, ["Java", "Kotlin", "Scala"], "pom.xml")
 
             # build.gradle (gradle)
-            repo.build_gradle_files = find_files(github_client, repo, ["Java", "Kotlin", "Scala"], "build.gradle")
+            repo.build_gradle_files = find_files(repo, ["Java", "Kotlin", "Scala"], "build.gradle")
 
             # requirements.txt (pip)
-            repo.requirements_txt_files = find_files(github_client, repo, ["Python"], "requirements.txt")
+            repo.requirements_txt_files = find_files(repo, ["Python"], "requirements.txt")
 
             # Gemfile (bundler)
-            repo.gemfile_files = find_files(github_client, repo, ["Ruby"], "Gemfile")
+            repo.gemfile_files = find_files(repo, ["Ruby"], "Gemfile")
 
             # Cargo.toml (cargo)
-            repo.cargo_toml_files = find_files(github_client, repo, ["Rust"], "Cargo.toml")
+            repo.cargo_toml_files = find_files(repo, ["Rust"], "Cargo.toml")
 
             # composer.json (composer)
-            repo.composer_json_files = find_files(github_client, repo, ["PHP"], "composer.json")
+            repo.composer_json_files = find_files(repo, ["PHP"], "composer.json")
 
             # Dockerfile (docker)
-            repo.dockerfile_files = find_files(github_client, repo, ["Dockerfile"], "Dockerfile")
+            repo.dockerfile_files = find_files(repo, ["Dockerfile"], "Dockerfile")
 
             # mix.exs (mix)
-            repo.mix_exs_files = find_files(github_client, repo, ["Elixir"], "mix.exs")
+            repo.mix_exs_files = find_files(repo, ["Elixir"], "mix.exs")
 
             # elm.json (elm)
-            repo.elm_json_files = find_files(github_client, repo, ["Elm"], "elm.json")
+            repo.elm_json_files = find_files(repo, ["Elm"], "elm.json")
 
             # .gitmodules (git submodules)
             # TODO: Match all languages
-            repo.gitmodules_files = find_files(github_client, repo, [""], ".gitmodules")
+            repo.gitmodules_files = find_files(repo, [""], ".gitmodules")
 
             # .github/workflows/*.yaml (github actions)
             # TODO: Match all languages
-            repo.github_workflows_files = find_files(github_client, repo, [""], ".github/workflows")
+            repo.github_workflows_files = find_files(repo, [""], ".github/workflows")
 
             # go.mod (gomod)
-            repo.go_mod_files = find_files(github_client, repo, ["Go"], "go.mod")
+            repo.go_mod_files = find_files(repo, ["Go"], "go.mod")
 
             # *.csproj (nuget)
             # TODO: Potentially match more languages
             # TODO: Check whether this is the correct filename
             # TODO: Match with regex
-            repo.csproj_files = find_files(github_client, repo, ["C#"], "*.csproj")
+            repo.csproj_files = find_files(repo, ["C#"], "*.csproj")
 
             # pubspec.yaml (pub)
-            repo.pubspec_yaml_files = find_files(github_client, repo, ["Dart"], "pubspec.yaml")
+            repo.pubspec_yaml_files = find_files(repo, ["Dart"], "pubspec.yaml")
 
             # *.tf (terraform)
             # TODO: Match with regex
-            repo.tf_files = find_files(github_client, repo, ["Terraform"], "*.tf")
+            repo.tf_files = find_files(repo, ["Terraform"], "*.tf")
 
             # Get info whether repo already has Dependabot config
-            repo.dependabot_config_files = find_files(github_client, repo, [""], ".github/dependabot.yml")
+            repo.dependabot_config_files = find_files(repo, [""], ".github/dependabot.yml")
             repo.has_dependabot_config = len(repo.dependabot_config_files) > 0
 
             # TODO: Get info whether Dependabot has opened PRs
             #repo_info.get_pulls
 
             # Get info whether repo has Renovate config
-            repo.renovate_config_files = find_files(github_client, repo, [""], ".github/renovate.json")
+            repo.renovate_config_files = find_files(repo, [""], ".github/renovate.json")
             repo.has_renovate_config = len(repo.renovate_config_files) > 0
 
             # Get info on activity of repo
@@ -108,29 +101,18 @@ def read_advanced_repo_info(github_client: Github, repos):
             
             repo.has_advanced_info = True
         else:
-            print(f"Advanced info for repo {repo.full_name} already read")
+            print(f"Advanced info for repo {repo.full_name} already read or no relevant languages found")
         
 
-# TODO: Match with regex
-def find_files(github_client, repo, languages, filename):
-    # TODO: Use downloaded files instead of API
+def find_files(repo, languages, filename):
     # Check if repo uses any of the given languages
     languages_intersection = [language for language in languages if language in repo.languages]
     if len(languages_intersection) > 0: 
-        # search for filename
-        result = github_client.search_code(query=f"repo:{repo.full_name} filename:{filename}")
-        relevant_files = []
-        count = 0
-        for file in result:
-            if file.path == filename or file.path.endswith("/"+filename):
-                print(f"Found filename for repo {repo.full_name} at /{file.path}")
-                relevant_files.append("/"+file.path)
-            count += 1
-            if count > 50:
-                # No more than 50 results
-                break
-            # To avoid rate limit
-            time.sleep(2)
-        return relevant_files
+        # use pathlib to search directory for filename
+        folder_path = f"repos/{repo.full_name.replace('/', '_')}"
+        results = pathlib.Path(folder_path).glob(f"**/{filename}")
+        results = [str(result) for result in results]
+        print(f"Found {languages_intersection} files for repo {repo.full_name} at {results}")
+        return results
     else:
         return []
