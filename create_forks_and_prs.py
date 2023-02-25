d6431a4cfd7b3d54890d9fcaed1f0bd515dfdbb8
Simon Convent <simon@convnet.de>
2023-02-25 23:46:06 +0100

    First implementation of forking

--- create_forks_and_prs.py
+++ create_forks_and_prs.py
@@ -1,2 +1,20 @@
+import time
+
 def create_forks_and_prs(github_client, repos):
-    print("Not yet implemented")
+    for repo in repos.values():
+        
+        # Check if fork already exists
+        try:
+            potentially_existing_fork = github_client.get_repo(f"sconvnet/{repo.name}")
+            print("Fork already exists")
+            return
+        except:
+            pass
+
+        # If not, create fork
+        print(f"Creating fork for {repo.full_name}")
+        github_repo = github_client.get_repo(repo.full_name)
+        github_repo.create_fork()
+
+        # Wait 60 seconds
+        time.sleep(60)
