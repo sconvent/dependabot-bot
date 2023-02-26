5d5516f799768ce207a0de1cd4f94a5d467c7fa0
Simon Convent <simon@convnet.de>
2023-02-26 23:57:37 +0100

    Create branch and dependabot file

--- create_forks_and_prs.py
+++ create_forks_and_prs.py
@@ -5,7 +5,7 @@ def create_forks_and_prs(github_client, repos):
         
         # Check if fork already exists
         try:
-            potentially_existing_fork = github_client.get_repo(f"sconvnet/{repo.name}")
+            potentially_existing_fork = github_client.get_repo(f"sconvent/{repo.name}")
             print("Fork already exists")
             return
         except:
@@ -15,6 +15,20 @@ def create_forks_and_prs(github_client, repos):
         print(f"Creating fork for {repo.full_name}")
         github_repo = github_client.get_repo(repo.full_name)
         github_repo.create_fork()
+
+        # Get fork
+        fork = github_client.get_repo(f"sconvent/{repo.name}")
+
+        # Create branch
+        print(f"Creating branch for {repo.full_name}")
+        fork.create_git_ref(ref=f"refs/heads/add-dependabot", sha=repo.sha)
+
+        # Create dependabot.yml
+        print(f"Creating dependabot.yml for {repo.full_name}")
+        fork.create_file(path="dependabot.yml", message="Add dependabot.yml", content="TBD", branch="add-dependabot")
+
+        # Create pull request
+        # Not yet implemented
 
         # Wait 60 seconds
         time.sleep(60)
