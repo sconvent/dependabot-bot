a2bfe42c81637023657c47b85c6408eb270602be
Simon Convent <simon@convnet.de>
2023-03-07 22:11:56 +0100

    Create content of dependabot.yml for some ecosystems

--- create_forks_and_prs.py
+++ create_forks_and_prs.py
@@ -1,6 +1,29 @@
 import time
+import pystache
+
+def extract_path(full_path):
+    return "/"+"/".join(full_path.split("/")[2:-1])
+
+
+def render_dependabot_config(configs):
+    return pystache.render(
+        """
+version: 2
+updates:
+{{#configs}}
+  - package-ecosystem: "{{ecosystem}}"
+    directory: "{{directory}}"
+    schedule:
+      interval: "weekly"
+
+{{/configs}}
+        """,
+        {'configs': configs},
+    )
 
 def create_forks_and_prs(github_client, repos):
+    # Todo: Implement dry run
+
     for repo in repos.values():
         
         # Check if fork already exists
@@ -26,7 +49,16 @@ def create_forks_and_prs(github_client, repos):
 
         # Create dependabot.yml
         print(f"Creating dependabot.yml for {repo.full_name}")
-        fork.create_file(path="dependabot.yml", message="Add dependabot.yml", content="TBD", branch="add-dependabot")
+        # Todo: Add more ecosystems
+        configs = \
+        [{'ecosystem': 'maven', 'directory': extract_path(path)} for path in repo.pom_xml_files] \
+        + [{'ecosystem': 'gradle', 'directory': extract_path(path)} for path in repo.build_gradle_files] \
+        + [{'ecosystem': 'npm', 'directory': extract_path(path)} for path in repo.package_json_files]
+        content = render_dependabot_config(configs)
+        #print("CONTENT:")
+        #print(content)
+        #print("END CONTENT")
+        fork.create_file(path="dependabot.yml", message="Add dependabot.yml", content=content, branch="add-dependabot")
 
         # Create pull request
         # Not yet implemented
